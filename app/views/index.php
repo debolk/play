b0eae688def3adb5fd6c98a3af56c12ab5edc795
Jakob Buis <jpabuis@gmail.com>
2013-11-07 16:19:19 +0100

    added jquery locally

--- app/views/index.php
+++ app/views/index.php
@@ -55,7 +55,7 @@
     </div>
 
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/konami.js"></script>
     <script src="js/index.js"></script>
