bf97eaabeea5864d26795fb9f088cd593ea6c384
Jakob Buis <jpabuis@gmail.com>
2013-11-07 15:12:23 +0100

    store correct time

--- app/controllers/FrontController.php
+++ app/controllers/FrontController.php
@@ -30,7 +30,7 @@ class FrontController extends BaseController
     $game = Game::findOrFail(Input::json('game_id'));
 
     if (Input::json('playing') && !$player->games->contains($game->id)) {
-      $player->games()->attach($game->id, ['created_at' => 'now', 'updated_at' => 'now']);
+      $player->games()->attach($game->id, ['created_at' => DB::raw('datetime()'), 'updated_at' => DB::raw('datetime()')]);
     }
     else {
       $player->games()->detach($game->id);
