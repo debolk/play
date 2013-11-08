1f9bf447546a7df5e4bf7538fbd0f009ad749bca
Jakob Buis <jpabuis@gmail.com>
2013-11-08 10:26:12 +0100

    extracted GameController from FrontController

--- app/controllers/FrontController.php
+++ app/controllers/FrontController.php
@@ -6,47 +6,4 @@ class FrontController extends BaseController
   {
     return View::make('index');
   }
-
-  public function addgame()
-  {
-    Game::create(['name' => Input::json('name')]);
-  }
-
-  public function destroy_game()
-  {
-    $player = Player::findOrFail(Input::json('player_id'));
-    $game = Game::findOrFail(Input::json('game_id'));
-
-    if (!$player->admin) {
-      return Response::make('Not authorized', 403);
-    }
-
-    $game->delete();
-  }
-
-  public function gamestate($player_id)
-  {
-    // Log player contact
-    $player = Player::findOrFail($player_id);
-    $player->touch();
-
-    // Purge offline players
-    Player::purge_afk();
-
-    // Return the gamestate
-    return View::make('games', ['games' => Game::all(), 'player' => $player]);
-  }
-
-  public function set_game()
-  {
-    $player = Player::findOrFail(Input::json('player_id'));
-    $game = Game::findOrFail(Input::json('game_id'));
-
-    if (Input::json('playing') && !$player->games->contains($game->id)) {
-      $player->games()->attach($game->id, ['created_at' => DB::raw('datetime()'), 'updated_at' => DB::raw('datetime()')]);
-    }
-    else {
-      $player->games()->detach($game->id);
-    }
-  }
 }
--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,13 @@
 <?php
 
+// Main interface
 Route::get('/', 'FrontController@index');
-Route::get('/gamestate/{player_id}', 'FrontController@gamestate');
-Route::post('/set_game', 'FrontController@set_game');
-Route::post('/addgame', 'FrontController@addgame');
-Route::delete('/destroygame', 'FrontController@destroy_game');
+
+// Managing games
+Route::get('/gamestate/{player_id}', 'GameController@gamestate');
+Route::post('/set_game', 'GameController@set_game');
+Route::post('/addgame', 'GameController@addgame');
+Route::delete('/destroygame', 'GameController@destroy_game');
 
 // Managing players
 Route::post('/promote/{id}', 'PlayerController@promote');
